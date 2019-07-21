a259ee0719d4782b84a03393a9c7e75495258737
Javier Gómez <jgomez@ccxc.us>
2019-07-21 10:29:00 -0500

    [add] auth complete

--- src/index.js
+++ src/index.js
@@ -5,10 +5,21 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const passport = require('passport');
 
+const users = require('./routes/user');
+
+mongoose.connect(process.env.DB_HOST, { useNewUrlParser: true })
+    .then(() => console.log('Database is connected'))
+    .catch(err => console.log(`Can not connect to the database: ${err}`));
+
 const app = express();
+
+app.use(passport.initialize());
+require('./passport')(passport);
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+app.use('/api/users', users);
 
 app.get('/', (req, res) => {
     res.send('Hi!');
